21ef722959f2b50dc4dfc86d68ab08e19aec6aee
H-H Moon <moonssang031@naver.com>
2021-04-20 11:12:13 +0900

    compass activity using button

---
+++ app/src/main/java/com/smuexample/compassappwibtn/CompassActivity.java
@@ -0,0 +1,14 @@
+package com.smuexample.compassappwibtn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class CompassActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
